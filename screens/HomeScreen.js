1881271b1d35f53be8f137cb9e0103c2619848f4
sauravDutt <sauravd069@gmail.com>
2022-08-06 16:00:55 +0530

    Just added some more stuff

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,56 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { SafeAreaView, Text, View, Image, TextInput, ScrollView } from 'react-native';
+import React, {useLayoutEffect} from 'react';
+import {useNavigation} from '@react-navigation/native';
+import {
+    UserIcon,
+    ChevronDownIcon,
+    SearchIcon,
+    AdjustmentsIcon
+} from 'react-native-heroicons/outline'
 
 const HomeScreen = () => {
+    const navigation = useNavigation();
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerShown: false,
+        })
+    }, [])
+
   return (
-    <View>
-      <Text className='text-red-500'>HomeScreen</Text>
-    </View>
+    <SafeAreaView className='bg-white pt-5'>
+      
+        {/* Header */}
+        <View className='flex-row pb-3 items-center mx-4 space-x-2 px-2'>
+            <Image 
+                source={require('../assets/one.png')}
+                className='h-10 w-10 bg-white-300 p-6 '
+            />
+            <View className='flex-1'>
+                <Text className='font-bold text-gray-400 text-xs'>
+                    Deliver Now!
+                </Text>
+                <Text className='font-bold text-xl'>
+                    Current Location
+                    <ChevronDownIcon size={20} color='#00CCBB'/>
+                </Text>
+            </View>
+            <UserIcon size={35} color='#00CCBB' />
+            
+        </View>
+        {/* SearchBar */}
+        <View className='flex-row items-center space-x-2 pb-2 mx-4 px-2'>
+            <View className='flex-row flex-1 space-x-2 bg-gray-200 p-3'>
+                <SearchIcon color='gray' size={20}/>
+                <TextInput placeholder='Restaurant or Cuisine ...' keyboardType='default'/>
+            </View>
+            <AdjustmentsIcon color='#00CCBB'/>
+        </View>
+
+        <ScrollView>
+            
+        </ScrollView>
+    </SafeAreaView>
   )
 }
 
