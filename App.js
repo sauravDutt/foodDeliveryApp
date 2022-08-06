e940778b7fb8ef9f48f458bf50bc769a7722f96a
sauravDutt <sauravd069@gmail.com>
2022-08-06 14:41:21 +0530

    Just added some more stuff

--- App.js
+++ App.js
@@ -1,18 +1,22 @@
-import { StyleSheet, View } from 'react-native';
+import { TailwindProvider } from 'tailwindcss-react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Text, SafeAreaView } from 'react-native';
+import HomeScreen from './screens/HomeScreen';
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      
-    </View>
+    <NavigationContainer>
+      <TailwindProvider>
+      <Stack.Navigator>
+        {/* Screens */}
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+      </TailwindProvider>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
