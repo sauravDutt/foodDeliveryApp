b810d02cca42fe6dd125d166dd7303e375417dd5
sauravDutt <sauravd069@gmail.com>
2022-08-07 16:38:56 +0530

    Just addeed some more stuff

--- components/FeaturedRow.js
+++ components/FeaturedRow.js
@@ -1,10 +1,42 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView} from 'react-native'
 import React from 'react'
+import { ArrowRightIcon } from 'react-native-heroicons/outline'
+import RestaurantCard from './RestaurantCard'
 
 const FeaturedRow = ({id, title, description}) => {
   return (
     <View>
-      <Text>FeaturedRow</Text>
+        <View className="mt-4 flex-row items-center justify-between px-4">
+        <Text className="font-bold text-lg">{title}</Text>
+        <ArrowRightIcon color="#00CCBB"/>
+        </View>
+
+        <Text className="text-xs text-gray-500 px-4">
+            {description}
+        </Text>
+
+        <ScrollView
+        horizontal
+        contentContainerStyle = {{
+            paddingHorizontal: 15,
+        }}
+        showsHorizontalScrollIndicator={false}
+        className = "pt-4"
+        >
+            {/* RestaurantCards */}
+            <RestaurantCard
+                id=""
+                imgUrl=""
+                title=""
+                rating=""
+                genre=""
+                address=""
+                short_description=""
+                dishes=""
+                long=""
+                lat=""
+            />
+        </ScrollView>
     </View>
   )
 }
