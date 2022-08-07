4403745933e709956d1ff1a08aeb6e395a0adfcc
sauravDutt <sauravd069@gmail.com>
2022-08-07 23:44:25 +0530

    Just added some more stuff

--- components/FeaturedRow.js
+++ components/FeaturedRow.js
@@ -25,16 +25,16 @@ const FeaturedRow = ({id, title, description}) => {
         >
             {/* RestaurantCards */}
             <RestaurantCard
-                id=""
-                imgUrl=""
-                title=""
-                rating=""
-                genre=""
-                address=""
-                short_description=""
-                dishes=""
-                long="" 
-                lat=""
+                id={123}
+                imgUrl='https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F19%2F2014%2F07%2F10%2Fpepperoni-pizza-ck-x.jpg&q=60' 
+                title="Pizza Corner"
+                rating={4.5}
+                genre="Italian"
+                address="DLF phase 5"
+                short_description="Original Italian"
+                dishes={[]}
+                long={29}
+                lat={45}
             />
         </ScrollView>
     </View>
